555081eb584a881b8f0e72c29dde766908916f51
rdamyanova <dmnvrmn@gmail.com>
2021-02-08 15:22:02 +0200

    Skeleton without Commands created

---
+++ WIM14/WIM14/Models/Contracts/ITeam.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WIM14.Models.Contracts
+{
+    interface ITeam
+    {
+    }
+}
---
+++ WIM14/WIM14/Models/WorkItems/Feedback.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WIM14.Models.WorkItems
+{
+    class Feedback
+    {
+    }
+}
