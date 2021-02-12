240b6089bbbe9e9cac2c47d1844b334bde856126
Rumyana Damyanova <dmnvrmn@gmail.com>
2021-02-12 11:39:15 +0000

    Fixed the ShowTeamActivity method in Team class, added ActivityHistory property in IMember and Member class.

--- WIM14/WIM14/Models/Contracts/IMember.cs
+++ WIM14/WIM14/Models/Contracts/IMember.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using WIM14.Core;
+using Type = WIM14.Models.Enums.Type;
 
 namespace WIM14.Models.Contracts
 {
     public interface IMember
     {
+        public string Name { get; }
+        public Type Type { get; }
+        public List<HistoryEntry> ActivityHistory { get; }
+        public string ShowInfo();
+        public string ShowActivityHistory();
+        public void AddHistoryEntry(string desc);
+
+
     }
 }
--- WIM14/WIM14/Models/Structure/Board.cs
+++ WIM14/WIM14/Models/Structure/Board.cs
@@ -1,15 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using WIM14.Core;
 using WIM14.Models.Contracts;
 using Type = WIM14.Models.Enums.Type;
 
 namespace WIM14.Models
 {
-    class Board : IBoard, IType
+    public class Board : IBoard
     {
-        private Type _type;
+        private readonly List<IWorkItems> workItems = new List<IWorkItems>(); // mislq, che trqbva da e ot WorkItems, a ne IWorkItems
+        private readonly List<HistoryEntry> activityHistory = new List<HistoryEntry>();
+        private string name;
+        public Board(string newName)
+        {
+            this.Name = newName;
+            this.Type = Type.Board;
+            this.AddHistoryEntry($"{this.Type} with name {this.Name} was created.");
+        }
+        public string Name 
+        {
+            get => this.name;
+            set
+            {
+                this.EnsureValidName(value);
+                this.name = value;
+                this.AddHistoryEntry($"{this.Type} name was changed to {value}.");
+            }
+        }
 
-        Type IType.Type => _type;
+        public Type Type
+        {
+            get;
+        }
+
+        public string ShowInfo()
+        {
+            return $"{this.Type} {this.Name} || Total work items: {this.workItems.Count} ";
+        }
+        public string ShowActivityHistory()
+        {
+            return string.Join(Environment.NewLine, this.activityHistory);
+        }
+        protected void AddHistoryEntry(string desc)
+        {
+            this.activityHistory.Add(new HistoryEntry(desc));
+        }
+        private void EnsureValidName(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Please provide a non-empty name.");
+            }
+            if (value.Length < 5 || value.Length > 15)
+            {
+                throw new ArgumentException("Please provide a name with length between 5 and 15 characters.");
+            }
+        }
     }
 }
